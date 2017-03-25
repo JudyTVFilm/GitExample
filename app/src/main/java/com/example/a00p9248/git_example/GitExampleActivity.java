600440191ec2de826af1b595190f69939b58389c
Judy Katz <jbkatz01@yahoo.com>
2017-03-25 09:09:04 -0700

    Add iml files

---
+++ app/src/main/java/com/example/a00p9248/git_example/GitExampleActivity.java
@@ -0,0 +1,13 @@
+package com.example.a00p9248.git_example;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GitExampleActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_git_example);
+    }
+}
